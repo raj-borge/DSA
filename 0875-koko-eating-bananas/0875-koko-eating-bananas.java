eacff02b7ed8525d7e5f37d9a6b37c22596c144f
raj-borge <rajborge3085@gmail.com>
2026-07-17 23:22:15 +0530

    Time: 15 ms (38.39%), Space: 47.3 MB (97.96%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,21 +1,13 @@
 class Solution {
-    private long calculateTotalHours(int[] piles,int speed){
-        long totalHrs=0;
-        for(int bananas:piles){
-            totalHrs+=(int)Math.ceil((double)bananas/speed);
-        }
-        return totalHrs;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-        int maxPile=Arrays.stream(piles).max().getAsInt();
         int low=1;
-        int high=maxPile;
-        int ans=maxPile;
+        int high=Arrays.stream(piles).max().getAsInt();
+        int ans=high;
+
         while(low<=high){
             int mid=low+(high-low)/2;
-            long time=calculateTotalHours(piles,mid);
 
-            if(time<=h){
+            if(canFinish(piles,h,mid)){
                 ans=mid;
                 high=mid-1;
             }
@@ -24,5 +16,14 @@ class Solution {
             }
         }
         return ans;
+    }
+
+    public boolean canFinish(int[] piles,int h,int k){
+        long hours=0;
+        for(int pile:piles){
+            hours+=pile/k;
+            if(pile %k !=0) hours++;
+        }
+        return hours<=h;
     }
 }
